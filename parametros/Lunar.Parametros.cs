c2e12d481342f31cc0cb6d807bc5f26c4e511e64
arkmol66-cmd <arkmol66@gmail.com>
2025-12-26 16:29:29 -0300

    Update Lunar.Parametros.cs

--- parametros/Lunar.Parametros.cs
+++ parametros/Lunar.Parametros.cs
@@ -1,21 +1,23 @@
+using System;
 using System.Collections.Generic;
 
 namespace Lunar.Lib {
     public class SistemaParametros {
-        public Dictionary<string, float> Valores = new Dictionary<string, float>();
-        public Dictionary<string, float> Maximos = new Dictionary<string, float>();
+        public Dictionary<string, float> Atributos = new Dictionary<string, float>();
+        public Dictionary<string, float> AtributosMax = new Dictionary<string, float>();
 
-        public void Criar(string nome, float valorInicial) {
-            Valores[nome] = valorInicial;
-            Maximos[nome] = valorInicial;
+        // Comando: parametro [nome] = [valor]
+        public void Definir(string nome, float valor) {
+            Atributos[nome] = valor;
+            AtributosMax[nome] = valor;
         }
 
-        public void Modificar(string nome, float qtd) {
-            if (Valores.ContainsKey(nome)) {
-                Valores[nome] += qtd;
-                if (Valores[nome] > Maximos[nome]) Valores[nome] = Maximos[nome];
-                if (Valores[nome] < 0) Valores[nome] = 0;
+        public void Modificar(string nome, float quantidade) {
+            if (Atributos.ContainsKey(nome)) {
+                Atributos[nome] = Math.Clamp(Atributos[nome] + quantidade, 0, AtributosMax[nome]);
             }
         }
+
+        public float Consultar(string nome) => Atributos.ContainsKey(nome) ? Atributos[nome] : 0;
     }
 }
